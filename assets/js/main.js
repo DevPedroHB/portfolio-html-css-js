5e02879666b60869ca93ce1d08d85bcb7e41555c
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2022-12-19 17:43:22 -0300

    Testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ modalCloses.forEach((modalClose) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
   navigation: {
@@ -112,7 +112,23 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    }
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
